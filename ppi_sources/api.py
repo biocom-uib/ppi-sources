bce66c83514fd4e6dc55d5308be67ba1357fa2df
Gabriel Riera <gabriel.riera@uib.es>
2021-03-17 12:40:58 +0100

    rework stringdb.api.build_custom_network and improve handling of error responses from the sources api

--- ppi_sources/api.py
+++ ppi_sources/api.py
@@ -6,6 +6,15 @@ import pandas as pd
 from yarl import URL
 
 
+class SourceAPIClientResponseError(ClientResponseError):
+    def __init__(self, request_info, history, json_body, *, status=None, message="", headers=None):
+        super().__init__(request_info, history, status=status, message=message, headers=headers)
+        self._json_body = json_body
+
+    @property
+    def json_body(self):
+        return self._json_body
+
 
 class SourceAPIClient(object):
     NO_TIMEOUT = ClientTimeout()
@@ -20,13 +29,50 @@ class SourceAPIClient(object):
         return self.host.with_path(path)
 
 
+    async def _raise_for_status_with_desc(self, resp):
+        # bug in aiohttp < 3.8: resp.ok calls raise_for_status which releases the response
+
+        # copied parts from 3.8 and inlined resp.ok from 3.8
+        if resp.status >= 400:
+            json_body = None
+
+            try:
+                json_body = await resp.json()
+            except e:
+                print('SourceAPIClient._raise_for_status_with_desc: error getting response body:', e)
+
+            # copied from aiohttp/client_reqrep.py @3.8
+            assert resp.reason is not None
+            resp.release()
+
+            raise SourceAPIClientResponseError(
+                resp.request_info,
+                resp.history,
+                json_body,
+                status=resp.status,
+                message=resp.reason,
+                headers=resp.headers)
+
+
     @asynccontextmanager
     async def request(self, method, path, **kwargs):
         url = self.complete_url(path)
 
+        raise_for_status = kwargs.pop('raise_for_status', False)
+
         try:
             async with self.client.request(method, url, **kwargs) as resp:
+                if callable(raise_for_status):
+                    raise_for_status(resp)
+                elif raise_for_status:
+                    await self._raise_for_status_with_desc(resp)
+
                 yield resp
+
+        except SourceAPIClientResponseError as exc:
+            print('Source API client HTTP error:', exc.status, exc.message, 'body:', exc.json_body)
+            raise
+
         except ClientResponseError as exc:
             print('HTTP error:', exc.status, exc.message)
             raise
--- ppi_sources/stringdb/api.py
+++ ppi_sources/stringdb/api.py
@@ -1,6 +1,7 @@
 from aiohttp import ClientSession, TCPConnector
 import numpy as np
 import pandas as pd
+import sys
 
 from ppi_sources.api import SourceAPIClient
 from ppi_sources.source import Source
@@ -43,30 +44,60 @@ class StringDBAPISource(Source):
 
         vert_ids = np.unique(edges_array.flatten()).tolist()
 
+        # assume that the user intended to use protein_id if given numeric IDs,
+        # protein_external_id otherwise
+        provided_stringdb_col = 'protein_external_id'
+
+        if all(vert_id.isnumeric() for vert_id in vert_ids):
+            provided_stringdb_col = 'protein_id'
+            vert_ids = [int(vert_id) for vert_id in vert_ids]
+
+        # request a mapping between protein_id and protein_external_id, also get related species_id's
         vert_data = await self.client.request_dataframe('POST', '/db/stringdb/items/proteins/select', json={
             'columns': ['protein_id', 'species_id', 'protein_external_id'],
             'filter': {
-                'protein_external_id': vert_ids
+                provided_stringdb_col: vert_ids
             }
         })
-        vert_data = vert_data.rename(columns={
+
+        stringdb_col_mapping = {
             'protein_id': 'string_id',
             'protein_external_id': 'external_id'
-        })
+        }
+        vert_data = vert_data.rename(columns=stringdb_col_mapping)
 
-        species_ids = vertices.species_id.unique().tolist()
+        species_ids = vert_data.species_id.unique().tolist()
 
+        # built the edge data frame
         edges_df = pd.DataFrame(edges_array, columns=['node_id_a', 'node_id_b'])
 
-        if len(ext_ids) < len(vert_ids):
-            # TODO: warn for missing ID's
-            vert_ids = set(ext_ids.external_id)
-            edges_df = edge_df.loc[edge_df.node_id_a.isin(vert_ids) & edge_df.node_id_b.isin(vert_ids)]
+        # ensure that all edges are known & mapped correctly to string_id's
+        old_nrows = len(edges_df)
 
-        name = f"stringdb_{species_id[0]}"
+        if provided_stringdb_col == 'protein_id':
+            known_ids = set(map(str, vert_data.string_id))
+
+            edges_df = edges_df.loc[
+                    edges_df.node_id_a.isin(known_ids)
+                    & edges_df.node_id_b.isin(known_ids)]
+        else:
+            ext_id_to_string_id = vert_data.set_index('external_id').string_id
+
+            edges_df = edges_df \
+                .apply(lambda col: col.map(ext_id_to_string_id)) \
+                .dropna()
+
+        if old_nrows != len(edges_df):
+            print(f'string.api.build_custom_network: dropped {old_nrows - len(edges_df)} unknown edges out of {old_nrows}',
+                file=sys.stderr)
+
+        name = "stringdb_custom_" + '_'.join(map(str, sorted(species_ids)))
         #name = await self.client.get_name(net_desc)
 
-        return StringDBNetwork(name, species_ids, ext_ids.set_index('string_id').external_id, edges_df)
+        return StringDBNetwork(name,
+                species_ids,
+                vert_data.set_index('string_id').external_id,
+                edges_df)
 
 
     async def get_bitscore_matrix(self, net1, net2):
